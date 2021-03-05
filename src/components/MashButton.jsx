6ef36d3e632308fce8158d5b2d38bb181b530dfb
Stephanie <Rinehart>
2021-03-05 17:01:36 -0500

    styling changes

--- src/components/MashButton.jsx
+++ src/components/MashButton.jsx
@@ -3,17 +3,20 @@ import styled from 'styled-components';
 
 const Button = styled.button`
     background: #7A63FF;
-    border-radius: 31px;
+    border-radius: 15px;
+    border: 3px solid #fff;
+    padding: 10px 50px;
+    margin-top: 20px;
     font-family: Sora;
-    font-size: 36px;
+    font-size: 24px;
     font-style: normal;
-    font-weight: 600;
-    line-height: 45px;
+    font-weight: 600; 
+    line-height: 45px; 
     letter-spacing: 0.04em;
     text-align: center;
     color: white;
-    width: 120px;
-    height: 91px;
+    ${'' /* box-shadow: 5px 5px 5px 1px rgba(255, 255, 255, .3); */}
+
 `
 
 export default function MashButton(props) {
