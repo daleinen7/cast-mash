693fa66d9258eaf27968d2dcfb9c001f3243a76a
daleinen7 <daleinen@gmail.com>
2021-03-05 14:04:38 -0600

    fcomponents in place with functionality

--- src/components/ActorSelect.jsx
+++ src/components/ActorSelect.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import defaultActor from '../static/images/spyman.png'
+import defaultActor from '../static/images/Vector.png'
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
-	background: green;
 
 `;
 
-export default function ActorSelect() {
-  return(
-    <Wrapper>
-      <img src="{ defaultActor }" alt="Placeholder"/>
-    </Wrapper>
-  )
+export default function ActorSelect(props) {
+  if (props.actorPic) {
+    return (
+      <Wrapper>
+          <img src={props.actorPic} alt="Actor"/>
+        <input type='text' className='actor1' onChange={props.handleChangeActor} />
+      </Wrapper>
+    )
+  } else {
+    return(
+      <Wrapper>
+          <img src={defaultActor} alt="default placeholder" />
+        <input type='text' className='actor1' onChange={props.handleChangeActor} />
+      </Wrapper>
+    )
+  }
 }
--- src/components/MashButton.jsx
+++ src/components/MashButton.jsx
@@ -2,11 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Button = styled.button`
-    background-color: yellow;
+    background: #7A63FF;
+    border-radius: 31px;
+    font-family: Sora;
+    font-size: 36px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 45px;
+    letter-spacing: 0.04em;
+    text-align: center;
+    color: white;
+    width: 120px;
+    height: 91px;
 `
 
-export default function MashButton() {
+export default function MashButton(props) {
   return(
-    <Button>Mash</Button>
+    <Button onClick={props.handleClick}>Mash</Button>
   )
 }
