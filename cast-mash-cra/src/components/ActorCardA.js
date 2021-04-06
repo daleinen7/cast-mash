32c0fc5d2fd13b18e92ca9c65658bc345f15e426
Noa Kim <noanonoa@gmail.com>
2021-04-05 22:46:44 -0700

    added actor card components

---
+++ cast-mash-cra/src/components/ActorCardA.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './ActorCard.css'
+import imagePlaceholder from '../static/02.png'
+
+function ActorCardA({ handleChangeActorA, actorAPicture, userInputActorA }) {
+  if (!actorAPicture) {
+    return (
+      <div className="actor-card">
+        <div className="actorImageHolder">
+          <img className="image-placeholder" src={ imagePlaceholder } alt="profile placeholder" />
+        </div>
+        <input type='text' onChange={ handleChangeActorA } />
+      </div> 
+    );
+  } else {
+    return (
+      <div className="actor-card">
+        <div className="actorImageHolder">
+          <img src={ actorAPicture } alt={ userInputActorA } />
+        </div>
+          <input type='text' onChange={ handleChangeActorA } />
+      </div>
+    );
+  }
+};
+
+export default ActorCardA;
---
+++ cast-mash-cra/src/components/ActorCardB.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './ActorCard.css'
+import imagePlaceholder from '../static/02.png'
+
+function ActorCardB({ handleChangeActorB, actorBPicture, userInputActorB }) {
+  if (!actorBPicture) {
+    return (
+      <div className="actor-card">
+        <div className="actorImageHolder">
+          <img className="image-placeholder" src={ imagePlaceholder } alt="profile placeholder" />
+        </div>
+        <input type='text' onChange={ handleChangeActorB } />
+      </div>
+    );
+  } else {
+    return (
+      <div className="actor-card">
+        <div className="actorImageHolder">
+          <img src={ actorBPicture } alt={ userInputActorB } />
+        </div>
+          <input type='text' onChange={ handleChangeActorB } />
+      </div>
+    );
+  }
+};
+
+export default ActorCardB;
