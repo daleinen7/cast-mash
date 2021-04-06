42e360cec397c5582a0ccb5a2b9687393057b92b
Noa Kim <noanonoa@gmail.com>
2021-04-05 20:48:46 -0700

    App component styling fixes

--- cast-mash-cra/src/App.js
+++ cast-mash-cra/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import ApiTesting from './components/ApiTesting';
+import background from './static/01.png';
 
 function App() {
   return (
     <div className="App">
+      <div className="background-container">
+        <img className="background" src={background} alt="background" />
+      </div>
       <ApiTesting />
     </div>
   );
